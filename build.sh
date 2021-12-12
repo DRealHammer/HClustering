be8fc9a865b5e19c57ff673bab4e8286ef6057f7
Daniel Hammer <d.h.99@web.de>
2021-12-12 18:07:23 +0100

    remove old code

--- build.sh
+++
@@ -1,9 +0,0 @@
-if [ ! -d "build" ]; then
-	mkdir build
-	echo "Created build directory"
-fi
-
-cmake -S . -B build
-
-cmake --build build
-
--- include/translation/graphFormats.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <KaHIP/graph_io.h>
-#include <KaHIP/graph_access.h>
-#include <fstream>
-#include <sstream>
-
-
-void graphToMTX(graph_access& graph, const std::string outputFilename);
